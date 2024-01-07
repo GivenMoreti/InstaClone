782b0b9a5fbe6e99c62d9a380e0b24e9d79445db
Given <moretigiven@gmail.com>
2024-01-07 11:54:31 +0200

    Customnize Logo

--- pages/AuthPage/AuthForm.jsx
+++ pages/AuthPage/AuthForm.jsx
@@ -1,25 +1,67 @@
-import { Box, Image, Input, VStack } from "@chakra-ui/react";
-import React,{useState} from "react";
+import {
+  Box,
+  Button,
+  Image,
+  Input,
+  VStack,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 
 export const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
 
-
-
-
-
   return (
-    <Box border={"1 px solid gray"} borderRadius={4} padding={5}>
+    <Box border={"0.1px solid gray"} borderRadius={4} padding={5}>
       <VStack spacing={4}>
         {/* logo */}
-        <Image src="./BottomOcean.png" />
+        <Image src="./OceanBottomText.png" boxSize="150px" objectFit="cover" />
         <Input placeholder="email" type="email" fontSize={14} />
         <Input placeholder="password" type="password" fontSize={14} />
-      
-      {!isLogin?(
-        <Input placeholder="confirm password" type="password" fontSize={14} />
-      ): null}
-      
+
+        {!isLogin ? (
+          <Input placeholder="confirm password" type="password" fontSize={14} />
+        ) : null}
+        <Button width={"full"} colorScheme="gray" variant="outline">
+          {isLogin ? "Login" : "Sign up"}
+        </Button>
+        {/* -----OR---- */}
+        <Flex
+          alignItems={"center"}
+          justifyContent={"center"}
+          my={4}
+          gap={1}
+          w={"full"}
+        >
+          <Box flex={2} h={"1px"} bg={"gray.400"} />
+          <Text mx={1} color={"white"}>
+            OR
+          </Text>
+          <Box flex={2} h={"1px"} bg={"gray.400"} />
+        </Flex>
+        <Flex lignItems={"center"} justifyContent={"center"}>
+          <Image src="./google.png" w={5} alt="google logo" />
+          <Text mx={2} color={"blue.500"} cursor={"pointer"}>
+            Sign in with google
+          </Text>
+        </Flex>
+        {/* sign up if you dont have account */}
+
+        <Flex lignItems={"center"} justifyContent={"center"}>
+          <Box mx={2} fontSize={14}>
+            {isLogin ? "Don't have account?" : "already have account"}
+          </Box>
+          <Box
+            mx={2}
+            fontSize={14}
+            color={"blue.500"}
+            cursor={"pointer"}
+            onClick={() => setIsLogin(!isLogin)}
+          >
+            {isLogin ? "Sign up" : "login"}
+          </Box>
+        </Flex>
       </VStack>
     </Box>
   );
