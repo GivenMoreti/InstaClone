264799fed524084adb134a25de9e2afa6488a759
Given <moretigiven@gmail.com>
2024-01-14 10:28:03 +0200

    Fix profile post

--- pages/ProfilePage/ProfilePost.jsx
+++ pages/ProfilePage/ProfilePost.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { Flex, Grid, GridItem } from "@chakra-ui/react";
+import React from "react";
 
 export const ProfilePost = () => {
   return (
-    <div>ProfilePost</div>
-  )
-}
+    <GridItem
+      cursor={"pointer"}
+      overflow={"hidden"}
+      border={"1px solid"}
+      position={"relative"}
+      borderColor={"whiteAlpha.300"}
+      aspectRatio={1 / 1}
+    >
+      <Flex
+        opacity={0}
+        _hover={{ opacity: 1 }}
+        position={"absolute"}
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        bg={"blackAlpha.700"}
+        transition={"all 0.3s ease"}
+        zIndex={1}
+        justifyContent={"center"}
+      >
+          <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+            <Flex>
+
+            </Flex>
+            <Flex></Flex>
+          </Flex>
+
+
+      </Flex>
+    </GridItem>
+  );
+};
--- pages/ProfilePage/ProfilePosts.jsx
+++ pages/ProfilePage/ProfilePosts.jsx
@@ -1,9 +1,11 @@
 import { Box, Grid, Skeleton, VStack } from '@chakra-ui/react'
 import React,{useState,useEffect} from 'react'
+import {ProfilePost} from './ProfilePost';
+
 
 export const ProfilePosts = () => {
 
-  const[isLoading,setIsLoading] = useState(true);
+  const[isLoading,setIsLoading] = useState(false);
   
   return (
     <Grid templateColumns={{sm:"repeat(1,1fr)",md:"repeat(3,1fr)"}}
@@ -19,6 +21,15 @@ export const ProfilePosts = () => {
           </Skeleton>
         </VStack>
       ))}
+
+      {/* if not loading */}
+      {
+        !isLoading && (
+          <>
+            <ProfilePost img="https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D" />
+          </>
+        )
+      }
 
 
     </Grid>
